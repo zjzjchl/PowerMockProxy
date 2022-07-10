b02f0f0ef65a3baced0b83104f4cd559efffc9cf
Hering <zjzjchl@126.com>
2022-07-10 17:54:34 +0800

    获取proto文件的服务和api树状结构

---
+++ src/biz/media/grpc_loader.js
@@ -0,0 +1,53 @@
+const protoLoader = require('@grpc/proto-loader');
+const grpcLibrary = require('@grpc/grpc-js');
+
+
+exports.isService= function(object) {
+  for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+      const element = object[key];
+      if (!element.path) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+exports.getApisOfService = function(object) {
+  let apis = [];
+  for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+      // 根据需要可以扩充字段
+      const element = object[key];
+      apis.push({name: key, type: 'grpc_api', path: element.path});
+    }
+  }
+  return apis;
+}
+
+exports.getTreeOfProtoFile = async function(protoFileName, options={}) {
+  let tree = [];
+  const packageDefinition = protoLoader.loadSync(protoFileName, options);
+  for (const key in packageDefinition) {
+    if (Object.hasOwnProperty.call(packageDefinition, key)) {
+      const element = packageDefinition[key];
+      // 协议描述
+      if (element.format || element.type){
+        continue;
+      }
+      // 是否是一个服务
+      if (exports.isService(element)) {
+        // _id, aid, type
+        let node = {name: key, children: [], type: 'grpc_service'};
+        let apis = exports.getApisOfService(element);
+        node.children = apis;
+        tree.push(node);
+      }
+    }
+  }
+  // console.log(JSON.stringify(tree));
+  return tree;
+}
+
+// exports.getTreeOfProtoFile("/root/PowerMockProxy/public/resources/62c42ef4281c4d161217bb15/greeter.proto");
