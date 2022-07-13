f04cfce1de014c39e484b59e94ea5f721d83bdd6
Hering <zjzjchl@126.com>
2022-07-13 17:11:49 +0800

    代码调整

--- src/biz/media/grpc_apis.js
+++ src/biz/media/grpc_apis.js
@@ -1,34 +1,25 @@
-// const protoLoader = require('@grpc/proto-loader');
 const grpcLibrary = require('@grpc/grpc-js');
 const { ApiGroup } = require('../../biz/app/index');
-const iconv = require('iconv-lite');
-const encoding = 'gbk';
-const binaryEncoding = 'binary';
 
-// const options = {};
 
 exports.makeGrpcRequest = async function(req, res) {
   console.log(req.body);
-  let content = {code: 2, error: '对应的记录不存在'};
+  let content = {code: 2, error: '对应的Api记录不存在!'};
   let group = await ApiGroup.getAPiGroup({query: {id: req.body.gid}}, {});
   if (group) {
-    // const packageDefinition = protoLoader.loadSync(group.path, options);
-    // const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition); 
     content = await new Promise((resolve, reject)=>{
       const client = new grpcLibrary.Client('192.168.192.81:30002', grpcLibrary.credentials.createInsecure());
       client.makeUnaryRequest(req.body.path, x=>x, x=>x, Buffer.from([]), (error, value)=>{
         // console.log(error, value);
         if (error) {
-          reject({code: 1});
+          reject({code: 1, error: 'powermock rpc调用失败', message: error});
         } else {
-          const content = iconv.decode(Buffer.from(value, binaryEncoding), encoding);
-          resolve({code: 0, content: content});
+          resolve({code: 0, content: value});
         }
       });
     });
-  //  console.log(content);
   }
-
+  console.log(content);
   return {aid: req.body.aid, gid: req.body.gid, path: req.body.path, content: content};
 }
 
