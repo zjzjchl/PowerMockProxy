cca6b8e9c0ce70bf229b5a7b514110d87675942a
Hering <zjzjchl@126.com>
2022-07-13 17:22:19 +0800

    错误提示

--- src/biz/media/grpc_apis.js
+++ src/biz/media/grpc_apis.js
@@ -7,17 +7,21 @@ exports.makeGrpcRequest = async function(req, res) {
   let content = {code: 2, error: '对应的Api记录不存在!'};
   let group = await ApiGroup.getAPiGroup({query: {id: req.body.gid}}, {});
   if (group) {
-    content = await new Promise((resolve, reject)=>{
-      const client = new grpcLibrary.Client('192.168.192.81:30002', grpcLibrary.credentials.createInsecure());
-      client.makeUnaryRequest(req.body.path, x=>x, x=>x, Buffer.from([]), (error, value)=>{
-        // console.log(error, value);
-        if (error) {
-          reject({code: 1, error: 'powermock rpc调用失败', message: error});
-        } else {
-          resolve({code: 0, content: value});
-        }
+    try {
+      content = await new Promise((resolve, reject)=>{
+        const client = new grpcLibrary.Client('192.168.192.81:30002', grpcLibrary.credentials.createInsecure());
+        client.makeUnaryRequest(req.body.path, x=>x, x=>x, Buffer.from([]), (error, value)=>{
+          // console.log(error, value);
+          if (error) {
+            reject({code: 1, error: 'powermock rpc调用失败', message: error});
+          } else {
+            resolve({code: 0, content: value});
+          }
+        });
       });
-    });
+    } catch (error) {
+      content = {...error};
+    }
   }
   console.log(content);
   return {aid: req.body.aid, gid: req.body.gid, path: req.body.path, content: content};
